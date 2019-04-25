efe14a6661124d2b76c4cebf6509a740964042ba
xiongmao <735159088@qq.com>
2019-04-25 22:44:51 +0800

    change sql

--- bishe.sql
+++ bishe.sql
@@ -11,11 +11,71 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 10/04/2019 17:25:50
+ Date: 25/04/2019 22:44:15
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_exam_judge
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_exam_judge`;
+CREATE TABLE `tb_exam_judge`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `exam_paper_id` int(11) NOT NULL COMMENT '所属试卷',
+  `exam_tittle` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '判断试题题目',
+  `exam_answer` int(255) NOT NULL COMMENT '判断试题答案：0表示错误，1表示正确',
+  `create_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_exam_paper
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_exam_paper`;
+CREATE TABLE `tb_exam_paper`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `exam_paper_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '试卷名称',
+  `exam_paper_type` int(10) NOT NULL COMMENT '试卷类型',
+  `invalid_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '失效时间',
+  `create_unit` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主板单位',
+  `responsible` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '责任人',
+  `status` tinyint(2) UNSIGNED ZEROFILL NOT NULL DEFAULT 00 COMMENT '试卷状态:1已发布，0未发布',
+  `create_at` timestamp(0) NOT NULL COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_exam_select
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_exam_select`;
+CREATE TABLE `tb_exam_select`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键Id',
+  `exam_paper_id` int(10) NOT NULL COMMENT '所属试卷',
+  `exam_tittle` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '试题题目',
+  `exam_parse` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '试题解析',
+  `exam_answer` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '试题答案',
+  `create_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tb_exam_select_option
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_exam_select_option`;
+CREATE TABLE `tb_exam_select_option`  (
+  `id` int(11) NOT NULL COMMENT '主键ID',
+  `exam_select` int(11) NOT NULL COMMENT '题目ID',
+  `option` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '选项',
+  `option_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '选项内容',
+  `create_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_at` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tb_role
@@ -25,7 +85,7 @@ CREATE TABLE `tb_role`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tb_user
@@ -39,7 +99,7 @@ CREATE TABLE `tb_user`  (
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
   `birthday` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '生日',
   `sex` tinyint(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT '性别:1代表男性，0代表女性',
-  `create_at` timestamp(0) NOT NULL COMMENT '创建时间',
+  `create_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_at` timestamp(0) NOT NULL COMMENT '更新时间',
   `delete_at` timestamp(0) NULL DEFAULT NULL COMMENT '删除时间',
   `is_delete` tinyint(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否可用：1代表可用，0代表已删除',
